6a24068abd4e4fc76ee39380cfaa899c92931453
soospitz <sks568@nyu.edu>
2020-12-29 15:58:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [status, setStatus] = useState([
+    { id: 1, state: "empty", clicked: false },
+    { id: 2, state: "empty", clicked: false },
+    { id: 3, state: "empty", clicked: false },
+    { id: 4, state: "empty", clicked: false },
+    { id: 5, state: "empty", clicked: false },
+    { id: 6, state: "empty", clicked: false },
+    { id: 7, state: "empty", clicked: false },
+    { id: 8, state: "empty", clicked: false },
+    { id: 9, state: "empty", clicked: false },
+  ]);
+  const [player, setPlayer] = useState("Player 1");
+  const [winner, setWinner] = useState("");
+
+  const handleClick = (id) => {
+    console.log("handle clicked");
+    console.log(id, "just id ");
+    let copyStatus = [...status];
+    copyStatus.forEach((item) => {
+      if (item.id === id && player === "Player 1" && item.clicked === false) {
+        item.state = "O";
+        item.clicked = true;
+        setPlayer("Player 2");
+      } else if (
+        item.id === id &&
+        player === "Player 2" &&
+        item.clicked === false
+      ) {
+        item.state = "X";
+        item.clicked = true;
+        setPlayer("Player 1");
+      }
+    });
+    setStatus(copyStatus);
+  };
+  const handleRestart = () => {
+    let copyStatus = [...status];
+    copyStatus.forEach((item) => {
+      item.state = "empty";
+      item.clicked = false;
+    });
+    setStatus(copyStatus);
+  };
+
+  const board = status.map((item) => {
+    return (
+      <div
+        className="item"
+        style={item.state === "X" ? { color: "#eb3c62" } : { color: "skyblue" }}
+        onClick={() => {
+          handleClick(item.id);
+        }}
+      >
+        {item.state === "empty" ? "" : item.state === "X" ? "X" : "O"}
+      </div>
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tic-Tac-Toe</h1>
+      <div className="container">
+        <div className="board">
+          {board}
+          <div className="restart" onClick={handleRestart}>
+            Restart
+          </div>
+        </div>
+        <div className="score">
+          <h3 style={player === "Player 1" ? { color: "skyblue" } : { color: "#eb3c62" }}>{player}</h3>
+          <h3>{winner} is the Winner!</h3>
+        </div>
+      </div>
     </div>
   );
 }
